1639b4834bcf8c840159b6779002b94a15fe29e7
heislercreative <brennan@heislercreative.com>
2018-04-24 06:36:16 -0600

    add Dog attributes

--- db/migrate/20180424122609_create_dogs.rb
+++ db/migrate/20180424122609_create_dogs.rb
@@ -1,6 +1,12 @@
 class CreateDogs < ActiveRecord::Migration[5.2]
   def change
     create_table :dogs do |t|
+      t.string :name
+
+      t.decimal :age
+
+      t.integer :breed_id
+      t.integer :user_id
 
       t.timestamps
     end
