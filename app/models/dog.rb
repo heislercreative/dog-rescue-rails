23191cbb5f56274d294def1106859f8e29afd41c
heislercreative <brennan@heislecreative.com>
2018-04-25 17:55:00 -0600

    add Dog attribute validations

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,4 +1,12 @@
 class Dog < ActiveRecord::Base
   belongs_to :user
   belongs_to :breed
+
+  validates_presence_of :name, :age, :trained, :fee
+  validate :gender_set?
+
+  def gender_set?
+    self.gender == 'm' || self.gender == 'f'
+  end
+
 end
