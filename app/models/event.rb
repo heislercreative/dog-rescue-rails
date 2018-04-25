ae7c6eedde80b5889eefc63ec6a4d23a020299a7
heislercreative <brennan@heislecreative.com>
2018-04-25 17:47:04 -0600

    add Event title & description length validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,6 @@ class Event < ActiveRecord::Base
   has_many :users, through: :user_events
 
   validates_presence_of :title, :description, :datetime
+  validates :title, length: { maximum: 40 }
+  validates :description, length: { minimum: 40 }
 end
