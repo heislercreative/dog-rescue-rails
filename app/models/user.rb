4fbd762120b1b801ef3be322e7f6e2cf21fac627
heislercreative <brennan@heislecreative.com>
2018-04-25 18:08:04 -0600

    add User presence validation & has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ActiveRecord::Base
   has_many :dogs
   has_many :user_events
   has_many :events, through: :user_events
+
+  has_secure_password
+  validates_presence_of :name, :email, :admin
+
+  def password_needed?
+    
+  end
 end
