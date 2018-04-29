6b1a533bdbdf18797f07356426db9734675cba72
heislercreative <brennan@heislercreative.com>
2018-04-29 17:50:56 -0600

    add Breed link & routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :dogs
   resources :users
   resources :events
+  resources :breeds
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
