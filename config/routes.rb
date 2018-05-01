16584cefbcb95eb054d6ac799a7e03a77db072ae
heislercreative <30783939+heislercreative@users.noreply.github.com>
2018-05-01 08:01:50 -0600

    remove nested Dog route & add nested Event route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :dogs, only: [:create, :index, :show, :edit, :update, :delete]
+  resources :dogs
   resources :users do
-    resources :dogs, only: [:new]
+    resources :events, only: [:new]
   end
-  resources :events
+  resources :events, only: [:create, :index, :show, :edit, :update, :delete]
   resources :breeds
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
