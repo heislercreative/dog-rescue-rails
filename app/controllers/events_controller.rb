0210efdf7df44485fc1373c573c7b4d577042fd5
heislercreative <brennan@heislecreative.com>
2018-05-01 17:47:54 -0600

    copy logic for EventsController & create nested routes

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,51 @@
+class EventsController < ApplicationController
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @user = User.find_by(id: params[:user_id])
+    @event = @user.events.build
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to event_path(@event)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find_by(id: params[:user_id])
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    @event.update(event_params)
+    if @event.save
+      redirect_to event_path(@event)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Event.find(params[:id]).destroy
+    redirect_to events_path
+  end
+
+  private
+
+  def event_params
+    params.require(:dog).permit(:name, :breed_name, :gender, :age, :trained, :fee, :description, :user_id)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :dogs
   resources :users do
-    resources :events, only: [:new]
+    resources :events, only: [:new, :create, :edit, :update, :delete]
   end
-  resources :events, only: [:create, :index, :show, :edit, :update, :delete]
-  resources :breeds
+  resources :events, only: [:index, :show]
+  resources :breeds, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
