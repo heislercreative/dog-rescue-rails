72e9745a09ea013ad68a9ce67d0610a352a3cf72
heislercreative <brennan@heislecreative.com>
2018-05-02 17:43:09 -0600

    add Breed routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
     resources :events, only: [:new, :create, :edit, :update, :delete]
   end
   resources :events, only: [:index, :show]
-  resources :breeds, only: [:show]
+  resources :breeds, only: [:show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
