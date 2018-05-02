a767142140633e86a1a259e302565d05454cbb90
heislercreative <30783939+heislercreative@users.noreply.github.com>
2018-05-01 19:37:01 -0600

    add organizer_id validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ActiveRecord::Base
   has_many :user_events
   has_many :users, through: :user_events
 
-  validates_presence_of :title, :description, :datetime
+  validates_presence_of :title, :description, :datetime, :organizer_id
   validates :title, length: { maximum: 40 }
   validates :description, length: { minimum: 40 }
 
