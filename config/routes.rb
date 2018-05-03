5ffe3904f0c3d6ed2866494b32c3e0302ebce298
heislercreative <brennan@heislecreative.com>
2018-05-02 19:38:32 -0600

    create Event show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :dogs
   resources :users do
-    resources :events, only: [:new, :update, :delete]
+    resources :events, only: [:new, :edit, :delete]
   end
   resources :events, only: [:index, :show, :create, :update]
   resources :breeds, only: [:show, :edit, :update, :destroy]
