3de38700d5fefed0e3d473eae6da63f9e98598a7
heislercreative <brennan@heislercreative.com>
2018-05-03 20:24:19 -0600

    update User routes, set signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  resources :dogs
 
-  resources :users do
+  get 'signup' => 'users#new'
+
+  resources :users, only:[:index, :show, :create, :update, :destroy] do
     resources :events, only: [:new, :edit, :destroy]
   end
 
   get 'events/past' => 'events#past'
   resources :events, only: [:index, :show, :create, :update]
 
+  resources :dogs
   resources :breeds, only: [:show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
