5896ea75e85ed8dd4fc7bfab651fa122834f6a0c
heislercreative <brennan@heislercreative.com>
2018-05-03 20:06:18 -0600

    create sign up form & logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,33 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
     @user = User.new
+    @user.admin = false
+  end
+
+  def create
+    raise params.inspect
+    if User.find(email: user_params[:email])
+      flash[:notice] = "Account already exists with this email. Please select a different email or sign in."
+      render :new
+    else
+      @user = User.new(user_params)
+      if @user.save
+        redirect_to user_path(@user)
+      else
+        render :new
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
