1153e618651462f0b686e9c45f87da7dde41c301
heislercreative <30783939+heislercreative@users.noreply.github.com>
2018-05-04 10:03:49 -0600

    remove titleize from breed_name

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -10,7 +10,7 @@ class Dog < ActiveRecord::Base
   end
 
   def breed_name=(name)
-    self.breed = Breed.find_or_create_by(name: name.titelize)
+    self.breed = Breed.find_or_create_by(name: name)
   end
 
   def breed_name
