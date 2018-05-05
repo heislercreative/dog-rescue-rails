405f0f60b741b79c13a4d5942145af1c7d86f6fd
heislercreative <brennan@heislecreative.com>
2018-05-04 20:21:40 -0600

    add flash-notice for login failures

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to user_path
+    if user = User.find_by(email: params[:email])
+      if user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect_to user_path
+      else
+        flash[:notice] = "Incorrect password. Please try again."
+        redirect_to login_path
+      end
     else
-      render :new
+      flash[:notice] = "No user exists with this email address."
+      redirect_to login_path
     end
   end
 
