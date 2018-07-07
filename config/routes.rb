4fee1fcb4007ab9dbab798a5abdbc2c4afd365d4
heislercreative <brennan@heislecreative.com>
2018-07-06 18:38:14 -0600

    reorder user_events routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,14 @@ Rails.application.routes.draw do
   root 'users#show'
   get 'signup' => 'users#new'
 
+  resource :user_events, only: [:create, :destroy]
+
   resources :users, only:[:index, :show, :create, :update, :destroy] do
     resources :events, only: [:index, :new, :show, :edit, :destroy]
   end
 
   get 'events/past' => 'events#past'
   resources :events, only: [:index, :create, :update]
-  resource :user_events, only: [:create, :destroy]
 
   resources :dogs
   get 'breeds/top/:rank' => 'breeds#top'
