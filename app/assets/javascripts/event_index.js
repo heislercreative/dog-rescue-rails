ce783dd02553ddc6520cc26c072c5efd21b28adf
heislercreative <brennan@heislecreative.com>
2018-07-07 15:53:23 -0600

    move Event index js to asset pipeline

---
+++ app/assets/javascripts/event_index.js
@@ -0,0 +1,15 @@
+$(function() {
+    $(".event-more").on("click", function() {
+      let eventId = $(this).data("id")
+      let organizerId = $(this).data("organizer")
+      $.getJSON(`/users/${organizerId}/events/${eventId}`, function(event) {
+        $(`.event-description-${eventId}`).text(event.description)
+        if (event.users.length !== 0) {
+          $(`.attending-div-${eventId}`).removeClass("hidden")
+          event.users.forEach(user =>
+            $(`.attending-list-${eventId}`).append(`<li>${user.name}</li>`)
+          )
+        }
+      })
+    })
+  })
