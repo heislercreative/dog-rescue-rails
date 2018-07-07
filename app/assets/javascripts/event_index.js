688f4147e962adf126d812719410b884e2477e5a
heislercreative <brennan@heislecreative.com>
2018-07-07 16:20:55 -0600

    hide "More" button on click

--- app/assets/javascripts/event_index.js
+++ app/assets/javascripts/event_index.js
@@ -1,16 +1,16 @@
 $(function() {
-    $(".event-more").on("click", function() {
-      let eventId = $(this).data("id")
-      let organizerId = $(this).data("organizer")
-      $.getJSON(`/users/${organizerId}/events/${eventId}`, function(event) {
-        $(`.event-description-${eventId}`).text(event.description)
-        if (event.users.length !== 0) {
-          $(`.attending-div-${eventId}`).removeClass("hidden")
-          event.users.forEach(user =>
-            $(`.attending-list-${eventId}`).append(`<li>${user.name}</li>`)
-          )
-        }
-        $(`button[data-id="${eventId}"]`).addClass("hidden")
-      })
+  $(".event-more").on("click", function() {
+    let eventId = $(this).data("id")
+    let organizerId = $(this).data("organizer")
+    $.getJSON(`/users/${organizerId}/events/${eventId}`, function(event) {
+      $(`.event-description-${eventId}`).text(event.description)
+      if (event.users.length !== 0) {
+        $(`.attending-div-${eventId}`).removeClass("hidden")
+        event.users.forEach(user =>
+          $(`.attending-list-${eventId}`).append(`<li>${user.name}</li>`)
+        )
+      }
+      $(`button[data-id="${eventId}"]`).addClass("hidden")
     })
   })
+})
