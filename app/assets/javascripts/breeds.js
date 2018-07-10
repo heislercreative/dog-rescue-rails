a46774bffb8a060f72871647de3f9604497ba00c
heislercreative <brennan@heislercreative.com>
2018-07-09 20:02:35 -0600

    move render logic to breeds.js

---
+++ app/assets/javascripts/breeds.js
@@ -0,0 +1,20 @@
+function Breed(attr) {
+  this.id = attr.id
+  this.name = attr.name
+  this.dogs = attr.dogs
+}
+
+Breed.prototype.renderLi = function() {
+  $("#breedList").append(
+    `<li><a href="/breeds/${this.id}">${this.name} (${this.dogs.length})</a></li>`
+  )
+}
+
+$("#breedList").html("")
+
+fetch('/breeds.json', { credentials: "same-origin"})
+  .then(resp => resp.json())
+  .then(json => json.forEach(function(b) {
+    let breed = new Breed(b)
+    breed.renderLi()
+  }))
